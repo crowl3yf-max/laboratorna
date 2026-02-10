c8ea0808eb827308319a50bcb31346c1ead7f654
Crowl <Crowl@DESKTOP-99U6VG3>
2026-02-10 15:38:59 +0200

    2 laba

---
+++ laba 1 C#/laba 1 C#/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+internal class Class1
+{
+    public string value;
+    public void show()
+    {
+        Console.WriteLine(value);
+    }
+    public int lenght()
+    {
+        return value.Length;
+    }
+    public void sort()
+    {
+        char[] chars = value.ToCharArray();
+        for (int i = 0; i < chars.Length - 1; i++)
+        {
+            for (int j = 0; j < chars.Length - i - 1; j++)
+            {
+                if (chars[j] > chars[j + 1])
+                {
+                    char temp = chars[j];
+                    chars[j] = chars[j + 1];
+                    chars[j + 1] = temp;
+                }
+            }
+        }
+        value = new string(chars);
+    }
+}
---
+++ laba 1 C#/laba 1 C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_1_C_
+{
+    internal class Program
+    {
+        static void Main()
+        { 
+            Class1 ryad = new Class1();
+            ryad.value = "zadacha";
+            ryad.show();
+            Console.WriteLine("Довжина: " + ryad.lenght());
+            ryad.sort();
+            Console.WriteLine("Відсортована: ");
+            ryad.show();
+        }
+    }
+}
